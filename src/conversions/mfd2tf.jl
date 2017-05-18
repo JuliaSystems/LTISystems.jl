ad9483555617d699eb63b74a5abcb3b716c9bf1c
Niklas Everitt <neveritt@kth.se>
2017-05-18 15:53:03 +0200

    Fixed bug in conversion from mfd2tf

--- src/conversions/mfd2tf.jl
+++ src/conversions/mfd2tf.jl
@@ -1,4 +1,4 @@
 tf(s::MFD{Val{:mimo}}) = tf(ss(s))
 
-tf(s::MFD{Val{:siso},Val{:cont}}) = tf(s.N[1], s.D[1])
-tf(s::MFD{Val{:siso},Val{:disc}}) = tf(s.N[1], s.D[1], s.Ts)
+tf(s::MFD{Val{:siso},Val{:cont}}) = tf(s.N, s.D)
+tf(s::MFD{Val{:siso},Val{:disc}}) = tf(s.N, s.D, s.Ts)
